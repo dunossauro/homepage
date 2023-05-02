6ccee8b11293bd5baef99c8ebfd47a2db6ea1c3b
dunossauro <mendesxeduardo@gmail.com>
2023-05-02 13:51:11 -0300

    Separando arquivos

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function showPix() {
+    var pix = document.getElementById("pix");
+    if (pix.style.display === "none") {
+	pix.style.display = "block";
+    } else {
+	pix.style.display = "none";
+    }
+}
